0aea11e3c86066b3d056a358c104e57639df6d15
Kamrul Hasan <kamrul444216@gmail.com>
2023-09-02 01:15:28 +0600

    Add stripe payment

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -2,20 +2,22 @@ import PageHeader from "../../Components/PageHeader/PageHeader";
 import ShopInstagram from "../../Components/ShopInstagram/ShopInstagram";
 import Subscribe from "../../Components/Subscribe/Subscribe";
 import useProducts from "../../hooks/useProducts";
-import { useCartContext} from "../../hooks/useCartContext";
+import { useCartContext } from "../../hooks/useCartContext";
 import { Link } from "react-router-dom";
 import { loadStripe } from "@stripe/stripe-js";
-
-
+import { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProviders";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-  
   const [products, loading] = useProducts();
-  const { cartItems, increaseQuantity, decreaseQuantity, deleteItem } = useCartContext();
-  
+  const {user} = useContext(AuthContext);
+  const { cartItems, increaseQuantity, decreaseQuantity, deleteItem } =
+    useCartContext();
+    const navigate = useNavigate();
 
   const calculateSubtotal = (product, quantity) => {
-    return (product.price * quantity);
+    return product.price * quantity;
   };
 
   const calculateTotal = () => {
@@ -30,60 +32,66 @@ const Cart = () => {
     return total;
   };
 
-
   // payment data
-  const paymentProductData = Object.keys(cartItems).map((product_id) => {
-    const cartItem = cartItems[product_id];
-    const product = products.find((p) => p._id === product_id);
-  
-    if (!product) {
-      return null;
-    }
-  
-    return {
-      id: product._id,
-      name: product.name,
-      price: product.price,
-      image: product.image,
-      quantity: cartItem,
-    };
-  }).filter((product) => product !== null);
-  
-  // console.log(paymentProductData);
+  const paymentProductData = Object.keys(cartItems)
+    .map((product_id) => {
+      const cartItem = cartItems[product_id];
+      const product = products.find((p) => p._id === product_id);
+
+      if (!product) {
+        return null;
+      }
+
+      return {
+        id: product._id,
+        name: product.name,
+        price: product.price,
+        image: product.image,
+        quantity: cartItem,
+      };
+    })
+    .filter((product) => product !== null);
+
+  console.log(paymentProductData);
 
   // payment integration
   const makePayment = async () => {
-    try {
-      const stripe = await loadStripe(import.meta.env.VITE_STRIPE);
-      const body = {
-        products: paymentProductData,
-      };
-      const headers = {
-        'Content-Type': 'application/json',
-      };
-      const response = await fetch('http://localhost:5000/api/create-checkout-session', {
-        method: 'POST',
+    const stripe = await loadStripe(import.meta.env.VITE_STRIPE);
+
+    const body = {
+      products: paymentProductData,
+    };
+    const headers = {
+      "content-type": "application/json",
+    };
+    const response = await fetch(
+      "http://localhost:5000/api/create-checkout-session",
+      {
+        method: "POST",
         headers: headers,
         body: JSON.stringify(body),
-      });
-  
-      const session = await response.json();
-      console.log('Session:', session);
-
-  
-      const result = await stripe.redirectToCheckout({
-        sessionId: session.id, // Pass the sessionId obtained from the response
-      });
-  
-      if (result.error) {
-        console.log(result.error);
       }
-    } catch (error) {
-      console.error('Error making payment:', error);
+    );
+
+    const session = await response.json();
+
+    const result = stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+
+    if (result.error) {
+      console.log(result.error);
     }
   };
 
-
+  const handleProceedToCheckout = async () => {
+    if(user?.email){
+      makePayment();
+    }else{
+      navigate('/login')
+      
+    }
+  }
 
   return (
     <div>
@@ -110,7 +118,6 @@ const Cart = () => {
                   Object.keys(cartItems).map((product_id) => {
                     const cartItem = cartItems[product_id];
                     const product = products.find((p) => p._id === product_id);
-                    
 
                     if (!product) {
                       return null; // Skip if product not found
@@ -118,29 +125,43 @@ const Cart = () => {
 
                     return (
                       <tr key={product._id} className="border-b border-t">
-                    <td className="md:flex gap-3 items-center">
-                      <img className="w-20" src={product.image} alt="" />
-                      <div>
-                        <p className="uppercase font-semibold">{product.name}</p>
-                        <p className="text-[#72AEC8]">${product.price}</p>
-                      </div>
-                    </td>
-                    <td className="">
-                      <div className="flex gap-3">
-                        <button onClick={()=>decreaseQuantity(product._id)} className=" w-6 md:w-10 flex items-center justify-center text-2xl shadow-[0_8px_30px_rgb(0,0,0,0.20)] border">
-                          -
-                        </button>
-                        <div className=" px-3 md:px-0 md:w-20 flex items-center justify-center text-2xl shadow-[0_8px_30px_rgb(0,0,0,0.20)] border">
-                          {cartItem}
-                        </div>
-                        <button onClick={()=>increaseQuantity(product._id)} className=" w-6 md:w-10 flex items-center justify-center text-2xl shadow-[0_8px_30px_rgb(0,0,0,0.20)] border">
-                          +
-                        </button>
-                      </div>
-                    </td>
-                    <td className="text-[#72AEC8] font-bold">${(product.price * cartItem).toFixed(2)}</td>
-                    <td><button onClick={()=>deleteItem(product._id)}>X</button></td>
-                  </tr>
+                        <td className="md:flex gap-3 items-center">
+                          <img className="w-20" src={product.image} alt="" />
+                          <div>
+                            <p className="uppercase font-semibold">
+                              {product.name}
+                            </p>
+                            <p className="text-[#72AEC8]">${product.price}</p>
+                          </div>
+                        </td>
+                        <td className="">
+                          <div className="flex gap-3">
+                            <button
+                              onClick={() => decreaseQuantity(product._id)}
+                              className=" w-6 md:w-10 flex items-center justify-center text-2xl shadow-[0_8px_30px_rgb(0,0,0,0.20)] border"
+                            >
+                              -
+                            </button>
+                            <div className=" px-3 md:px-0 md:w-20 flex items-center justify-center text-2xl shadow-[0_8px_30px_rgb(0,0,0,0.20)] border">
+                              {cartItem}
+                            </div>
+                            <button
+                              onClick={() => increaseQuantity(product._id)}
+                              className=" w-6 md:w-10 flex items-center justify-center text-2xl shadow-[0_8px_30px_rgb(0,0,0,0.20)] border"
+                            >
+                              +
+                            </button>
+                          </div>
+                        </td>
+                        <td className="text-[#72AEC8] font-bold">
+                          ${(product.price * cartItem).toFixed(2)}
+                        </td>
+                        <td>
+                          <button onClick={() => deleteItem(product._id)}>
+                            X
+                          </button>
+                        </td>
+                      </tr>
                     );
                   })
                 )}
@@ -150,20 +171,28 @@ const Cart = () => {
           <div className="uppercase">
             <h5 className="text-xl">Cart Totals</h5>
             <div className="flex justify-between md:justify-normal md:gap-56 border-t-2 pt-3 pb-3">
-                <p className="underline">Subtotal</p>
-                <p>${calculateTotal().toFixed(2)}</p>
+              <p className="underline">Subtotal</p>
+              <p>${calculateTotal().toFixed(2)}</p>
             </div>
             <div className="flex justify-between md:justify-normal md:gap-60 border-t-2 pt-3 border-b-2 pb-3">
-                <p className="underline">Total</p>
-                <p>${calculateTotal().toFixed(2)}</p>
+              <p className="underline">Total</p>
+              <p>${calculateTotal().toFixed(2)}</p>
             </div>
           </div>
 
           <div className="flex gap-3 pb-10">
             <Link to="/shop">
-            <button className="bg-black text-white px-5 py-2 mt-5 border border-black hover:text-black hover:bg-transparent transition-all duration-150 delay-150 uppercase">Continue shopping</button>
+              <button className="bg-black text-white px-5 py-2 mt-5 border border-black hover:text-black hover:bg-transparent transition-all duration-150 delay-150 uppercase">
+                Continue shopping
+              </button>
             </Link>
-            <button onClick={makePayment} type="button" className="bg-black text-white px-5 py-2 mt-5 border border-black hover:text-black hover:bg-transparent transition-all duration-150 delay-150 uppercase">Proceed to checkout</button>
+            <button
+              onClick={handleProceedToCheckout}
+              type="button"
+              className="bg-black text-white px-5 py-2 mt-5 border border-black hover:text-black hover:bg-transparent transition-all duration-150 delay-150 uppercase"
+            >
+              Proceed to checkout
+            </button>
           </div>
         </div>
       </div>
@@ -174,6 +203,3 @@ const Cart = () => {
 };
 
 export default Cart;
-
-
-
--- src/Pages/SingleProduct/SingleProduct.jsx
+++ src/Pages/SingleProduct/SingleProduct.jsx
@@ -6,28 +6,45 @@ import "swiper/css/pagination";
 // import required modules
 import { Pagination } from "swiper/modules";
 import { FaShoppingCart } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { useParams } from 'react-router';
+import { Link,useNavigate } from "react-router-dom";
+import { useParams } from "react-router";
 import Subscribe from "../../Components/Subscribe/Subscribe";
 import ShopInstagram from "../../Components/ShopInstagram/ShopInstagram";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import useProducts from "../../hooks/useProducts";
 import { useCartContext } from "../../hooks/useCartContext";
+import { AuthContext } from "../../Providers/AuthProviders";
+import { loadStripe } from "@stripe/stripe-js";
 const SingleProduct = () => {
   const [products] = useProducts();
-  const [product,setProduct] = useState();
+  const [product, setProduct] = useState();
   const [quantity, setQuantity] = useState(1);
+  const {user} = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  const {id} = useParams();
-  useEffect(()=>{
+  // Inside your SingleProduct component
+  const paymentProductData = [
+    {
+      id: product?._id,
+      name: product?.name,
+      price: product?.price,
+      image: product?.image,
+      quantity: quantity,
+    },
+  ];
+
+  const { id } = useParams();
+  useEffect(() => {
     fetch(`http://localhost:5000/products/${id}`)
-    .then(res => res.json())
-    .then(data => {
-      setProduct(data)
-    })
-  },[id])
-  const relatedData = products.filter(item => item?.category === product?.category)
-  const {addItemToCart} = useCartContext();
+      .then((res) => res.json())
+      .then((data) => {
+        setProduct(data);
+      });
+  }, [id]);
+  const relatedData = products.filter(
+    (item) => item?.category === product?.category
+  );
+  const { addItemToCart } = useCartContext();
 
   const handleIncrement = () => {
     setQuantity(quantity + 1);
@@ -43,9 +60,55 @@ const SingleProduct = () => {
       addItemToCart(product._id, quantity);
     }
   };
+  
+  
+
 
   
-  
+
+
+  // payment integration
+  const makePayment = async () => {
+    const stripe = await loadStripe(import.meta.env.VITE_STRIPE);
+
+    const body = {
+      products: paymentProductData,
+    };
+    const headers = {
+      "content-type": "application/json",
+    };
+    const response = await fetch(
+      "http://localhost:5000/api/create-checkout-session",
+      {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(body),
+      }
+    );
+
+    const session = await response.json();
+
+    const result = stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+
+    if (result.error) {
+      console.log(result.error);
+    }
+  };
+
+  const handleProceedToCheckout = async () => {
+    if(user?.email){
+      makePayment();
+    }else{
+      navigate('/login')
+      
+    }
+  }
+
+
+
+
   return (
     <div>
       <div className="max-w-screen-xl mx-auto px-3 md:px-0">
@@ -65,70 +128,92 @@ const SingleProduct = () => {
             </h3>
             <p>{product?.stock} in stock</p>
             <div className="flex gap-5">
-              <button onClick={handleDecrement} className="w-10 flex items-center justify-center text-2xl shadow-[0_8px_30px_rgb(0,0,0,0.20)] border">
+              <button
+                onClick={handleDecrement}
+                className="w-10 flex items-center justify-center text-2xl shadow-[0_8px_30px_rgb(0,0,0,0.20)] border"
+              >
                 -
               </button>
               <div className="w-20 flex items-center justify-center text-2xl shadow-[0_8px_30px_rgb(0,0,0,0.20)] border">
                 {quantity}
               </div>
-              <button onClick={handleIncrement} className="w-10 flex items-center justify-center text-2xl shadow-[0_8px_30px_rgb(0,0,0,0.20)] border">
+              <button
+                onClick={handleIncrement}
+                className="w-10 flex items-center justify-center text-2xl shadow-[0_8px_30px_rgb(0,0,0,0.20)] border"
+              >
                 +
               </button>
             </div>
             <div className="flex gap-5">
-              <button className="bg-[#72AEC8] text-white px-5 py-2 mt-5 border border-[#72AEC8] hover:text-black hover:bg-transparent transition-all duration-150 delay-150">
+              <button onClick={handleProceedToCheckout} className="bg-[#72AEC8] text-white px-5 py-2 mt-5 border border-[#72AEC8] hover:text-black hover:bg-transparent transition-all duration-150 delay-150">
                 BUY NOW
               </button>
-              <button onClick={handleAddToCart} className="bg-black text-white px-5 py-2 mt-5 border border-black hover:text-black hover:bg-transparent transition-all duration-150 delay-150">
+              <button
+                onClick={handleAddToCart}
+                className="bg-black text-white px-5 py-2 mt-5 border border-black hover:text-black hover:bg-transparent transition-all duration-150 delay-150"
+              >
                 ADD TO CART
               </button>
             </div>
             <p>Category: {product?.category}</p>
             <p>Brand: {product?.brand}</p>
-          </div>?
+          </div>
+          ?
         </div>
         <hr />
         <div className="py-5">
-          <h5 className="text-2xl pb-3 font-semibold">Product Details & Description</h5>
+          <h5 className="text-2xl pb-3 font-semibold">
+            Product Details & Description
+          </h5>
           <p>{product?.description}</p>
         </div>
         <hr />
         <div className="py-10">
-        <div className="flex justify-between py-5 uppercase">
-        <h5 className="text-2xl font-semibold">Related Products</h5>
-        <Link className="border-b-2 " to={'/shop'}>Go to Shop</Link>
-        </div>
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={30}
-          pagination={{
-            clickable: true,
-          }}
-          breakpoints={{
-            640: {
-              slidesPerView: 1,
-              spaceBetween: 20,
-            },
-            768: {
-              slidesPerView: 4,
-              spaceBetween: 40,
-            },
-          }}
-
-          modules={[Pagination]}
-          className="mySwiper"
-        >
-            {relatedData.map(product => (
-                <SwiperSlide key={product.id}>
+          <div className="flex justify-between py-5 uppercase">
+            <h5 className="text-2xl font-semibold">Related Products</h5>
+            <Link className="border-b-2 " to={"/shop"}>
+              Go to Shop
+            </Link>
+          </div>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+              },
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            {relatedData.map((product) => (
+              <SwiperSlide key={product._id}>
+                {" "}
+                {/* Use a unique key */}
                 <div className="flex flex-col gap-3 border py-10 relative group">
                   <div className="relative cursor-pointer transition-transform transform-gpu hover:scale-110">
                     <img className="mx-auto" src={product.image} alt="" />
                     <div className="absolute bottom-20 left-0 right-0 flex justify-center items-center opacity-0 transition-opacity duration-300 group-hover:opacity-100 flex-col gap-2">
-                          <Link to={`/products/${product._id}`}>
-                          <button className="flex text-xs border border-white items-center gap-1 bg-black text-white justify-center py-1 px-2 rounded max-w-[120px]">View Detail <FaEye/></button>
-                          </Link>
-                          <button className="flex text-xs border border-white items-center gap-1 bg-black text-white justify-center py-1 px-1 rounded max-w-[120px]">Add Cart <FaShoppingCart/></button>
-                        </div>
+                      <Link to={`/products/${product._id}`}>
+                        <button className="flex text-xs border border-white items-center gap-1 bg-black text-white justify-center py-1 px-2 rounded max-w-[120px]">
+                          View Detail <FaEye />
+                        </button>
+                      </Link>
+                      <button
+                        className="flex text-xs border border-white items-center gap-1 bg-black text-white justify-center py-1 px-1 rounded max-w-[120px]"
+                        onClick={() => handleAddToCart(product._id)}
+                      >
+                        Add Cart <FaShoppingCart />
+                      </button>
+                    </div>
                   </div>
                   <div className="flex justify-around">
                     <p className="font-bold uppercase">{product.name}</p>
@@ -137,11 +222,11 @@ const SingleProduct = () => {
                 </div>
               </SwiperSlide>
             ))}
-        </Swiper>
+          </Swiper>
         </div>
       </div>
-      <Subscribe/>
-      <ShopInstagram/>
+      <Subscribe />
+      <ShopInstagram />
     </div>
   );
 };
